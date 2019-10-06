5c94b7fea2ad0fb23f9a5241b617c8ddce6f538f
DTSDAO <dtsdao@aliyun.com>
2019-10-06 21:17:48 +0800

    improve

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,9 @@ cd web
 npm run build
 cd ..
 cp -r ./web/build/* ./deploy
+rm -rf ./web/build
 cd deploy
 git add .
-git commit -m `date "+[%Y-%m-%d %H:%M] release"`
+git commit -m "`date "+[%Y-%m-%d %H:%M] release"`"
 git push
 rm -rf `ls`
