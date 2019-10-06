29f9cb8cb7a56c7a6d577db699344f31ae4e52dd
DTSDAO <dtsdao@aliyun.com>
2019-10-06 21:10:03 +0800

    improve

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ echo "  Cytus II DB Migrate Tool V1.0  "
 echo "           A.R.C. Tech.          "
 echo "================================="
 
+cd web
+npm run build
+cd ..
 cp -r ./web/build/* ./deploy
 cd deploy
 git add .
